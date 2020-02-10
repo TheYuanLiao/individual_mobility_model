b39e4940c5396719fbd048822f38efb98af9d338
Eric Wennerberg <ericwenn@gmail.com>
2020-02-10 15:59:22 +0100

    group and stuff

--- src/go/cmd/userlocation/main.go
+++ src/go/cmd/userlocation/main.go
@@ -4,20 +4,26 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"sort"
+
+	"github.com/ericwenn/mscthesis/src/go/internal/twitterddl"
+
 	"github.com/mmcloughlin/geohash"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type UserTopLocation struct {
-	UserID  int
-	Geohash string // 6 precision
+	UserID      int
+	Geohash     string // 6 precision
 	TweetsInGeo int
 }
 
 type UserLoc struct {
-	UserID  int
-	Lat float64 // 6 precision
-	Long float64
+	UserID int
+	Lat    float64 // 6 precision
+	Long   float64
 }
 
 func (s *UserTopLocation) UnmarshalRow(row *sql.Rows) error {
@@ -30,26 +36,119 @@ func main() {
 	db, err := sql.Open("sqlite3", *sqlitePath)
 	defer db.Close()
 	check(err)
+	if _, err := db.Exec(twitterddl.HomeLocationDDL); err != nil {
+		log.Fatal(err)
+	}
 	topGeohash, err := listUsersTopGeohash(db)
 	check(err)
-	var userLocs []*UserLoc
-	for _, row := range topGeohash {
-		var u UserLoc
-		u.UserID = row.UserID
-		lat, long := geohash.DecodeCenter(row.Geohash)
-		u.Lat = lat
-		u.Long = long
-		userLocs = append(userLocs, &u)
-
+	usersGeohashes := groupByUser(topGeohash)
+	var hls []*twitterddl.HomeLocation
+	for _, tls := range usersGeohashes {
+		if len(tls) < 2 {
+			continue
+		}
+		hl := homeLocation(tls)
+		hls = append(hls, hl)
 	}
-	for _, u := range userLocs[0:100] {
-		fmt.Println(u)
+	check(insertHomeLocations(db, hls))
+}
+
+func insertHomeLocations(db *sql.DB, hls []*twitterddl.HomeLocation) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	stm, err := tx.Prepare(twitterddl.HomeLocationInsertStm)
+	if err != nil {
+		return err
+	}
+	defer stm.Close()
+	for _, hl := range hls {
+		if _, err := stm.Exec(hl.InsertExec()...); err != nil {
+			return err
+		}
+	}
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func groupByUser(tls []*UserTopLocation) map[int][]*UserTopLocation {
+	users := make(map[int][]*UserTopLocation)
+	for _, tl := range tls {
+		users[tl.UserID] = append(users[tl.UserID], tl)
+	}
+	return users
+}
+
+const usersToGeohashStm = `
+SELECT
+   user_id, 
+   geohash as geohash, 
+   count(*) as c
+FROM spatiotemporal
+WHERE             
+	  hourofday < 10      
+  and hourofday > 0    
+  and weekday < 5    
+  and weekday > 0   
+GROUP BY geohash, user_id   
+ORDER BY c desc
+`
+
+func homeLocation(tls []*UserTopLocation) *twitterddl.HomeLocation {
+	totalTweets := 0
+	for _, bin := range tls {
+		totalTweets += bin.TweetsInGeo
+	}
+	perc := float64(0)
+	for perc < 0.1 {
+		perc = float64(tls[0].TweetsInGeo) / float64(totalTweets)
+		// fmt.Println(len(tls), tls[0].TweetsInGeo, perc)
+		tls = unsharpen(tls)
+	}
+	lat, lng := geohash.Decode(tls[0].Geohash)
+	return &twitterddl.HomeLocation{
+		UserID:      tls[0].UserID,
+		Latitude:    lat,
+		Longitude:   lng,
+		Geohash:     tls[0].Geohash,
+		Percentage:  perc,
+		TotalTweets: totalTweets,
 	}
 }
 
+func unsharpen(tls []*UserTopLocation) []*UserTopLocation {
+	groups := make(map[string][]*UserTopLocation)
+	for _, tl := range tls {
+		if len(tl.Geohash) == 0 {
+			fmt.Println("Skipping nil geohash")
+			continue
+		}
+		unsharp := tl.Geohash[:len(tl.Geohash)-1]
+		groups[unsharp] = append(groups[unsharp], tl)
+	}
+	var newTls []*UserTopLocation
+	for hash, tls := range groups {
+		newCount := 0
+		for _, tl := range tls {
+			newCount += tl.TweetsInGeo
+		}
+		newTls = append(newTls, &UserTopLocation{
+			UserID:      tls[0].UserID,
+			Geohash:     hash,
+			TweetsInGeo: newCount,
+		})
+	}
+	sort.Slice(newTls, func(i, j int) bool {
+		return newTls[i].TweetsInGeo > newTls[j].TweetsInGeo
+	})
+	return newTls
+}
 
 func listUsersTopGeohash(db *sql.DB) ([]*UserTopLocation, error) {
-	stm, err := db.Prepare("SELECT user_id, geohash, max(c) FROM (     SELECT         user_id, substr(geohash, 0, 7) as geohash, count(*) as c     FROM spatiotemporal     WHERE             hourofday < 10       and hourofday > 0       and weekday < 5       and weekday > 0     GROUP BY substr(geohash, 0, 7), user_id     ORDER BY c desc) GROUP BY user_id, geohash order by max(c) desc")
+	stm, err := db.Prepare(usersToGeohashStm)
 	if err != nil {
 		return nil, err
 	}
