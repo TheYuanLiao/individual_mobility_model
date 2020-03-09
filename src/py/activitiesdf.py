6ae1b5d740ad515b17d6b677864cfcccb2e96a99
Kristoffer Ek <kristofferboek@gmail.com>
2020-03-09 13:06:16 +0100

    Multiple home locations

--- src/py/activitiesdf.py
+++ src/py/activitiesdf.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import geopandas as gpd
+from datetime import timedelta
 
 regions = {
     "sweden": "./../../dbs/activities/sweden.csv",
@@ -22,7 +23,7 @@ def read_csv(region="sweden"):
 def during_home(df):
     weekdays = (df['weekday'] < 6) & (0 < df['weekday'])
     weekends = (df['weekday'] == 6) | (0 == df['weekday'])
-    morning_evening = (df['hourofday'] < 10) | (17 < df['hourofday'])
+    morning_evening = (df['hourofday'] < 9) | (18 < df['hourofday'])
     return df[((weekdays) & (morning_evening)) | (weekends)]
 
 
@@ -34,13 +35,58 @@ def label_home(df):
     print("largest cluster")
     homeidx = during_home(dfc)\
         .groupby(['userid', 'region']).size() \
-        .groupby('userid').nlargest(1) \
+        .groupby('userid').nlargest(2) \
         .index.tolist()
     print(len(homeidx))
     print("set values")
     for (_, userid, regionid) in homeidx:
         dfc.loc[(userid, regionid), 'label'] = "home"
     return dfc
+
+def home_overlaps(h1, h2):
+    return h1['home_start'] < h2['home_end'] and h2['home_start'] < h1['home_end']
+
+def less_than_30_days(h):
+    return (h['home_end'] - h['home_start']) < timedelta(days=30)
+
+def label_multiple_homes(df):
+    print("copying")
+    acts_2 = df.copy(deep=True).reset_index()
+    print("reindex")
+    acts_2 = acts_2.set_index(['userid', 'region', 'tweetid']).sort_index()
+    print("largest cluster")
+    homeidx = during_home(acts_2) \
+        .groupby(['userid', 'region']).size() \
+        .groupby('userid').nlargest(3)
+    homeidx.index = homeidx.index.droplevel()
+    usr_3homeloc = acts_2.reset_index().set_index(['userid', 'region']).loc[homeidx.index.values]
+    usr_3homeloc = usr_3homeloc.reset_index().set_index(['userid', 'region']).sort_index()
+    home_start = pd.Series(index=homeidx.index, dtype='datetime64[ns]')
+    home_end = pd.Series(index=homeidx.index, dtype='datetime64[ns]')
+    for (u, r), size in homeidx.iteritems():
+        perc_10 = int(size / 10)
+        perc_10 = 1 if perc_10 < 1 else perc_10
+        hometweets = usr_3homeloc.loc[(u, r)].iloc[perc_10:-perc_10]
+        home_start.loc[(u, r)] = hometweets['createdat'].min()
+        home_end.loc[(u, r)] = hometweets['createdat'].max()
+    homes = pd.DataFrame({'size': homeidx, 'home_start': home_start, 'home_end': home_end})
+    for u, rows in homes.groupby(level=0):
+        hs = rows.sort_values('size', ascending=False)
+        if len(hs) < 2:
+            continue
+        if home_overlaps(hs.iloc[0], hs.iloc[1]) or less_than_30_days(hs.iloc[1]):
+            homes.drop(index=[hs.iloc[1].name], inplace=True)
+        if len(hs) < 3:
+            continue
+        if home_overlaps(hs.iloc[0], hs.iloc[2]) or home_overlaps(hs.iloc[1], hs.iloc[2]) or less_than_30_days(
+                hs.iloc[2]):
+            homes.drop(index=[hs.iloc[2].name], inplace=True)
+    homes = homes.dropna()
+    homes_list = homes.index.tolist()
+    print("set values")
+    for (userid, regionid) in homes_list:
+        acts_2.loc[(userid, regionid), 'label'] = "home"
+    return acts_2
 
 
 def during_work(df):
@@ -55,15 +101,18 @@ def label_work(df):
     for uid in dfc.index.get_level_values(0).unique():
         udfc = dfc.loc[uid, :]
         udf = udfc.copy(deep=True).reset_index()
-        home_region = udf[udf['label'] == 'home'].iloc[0]['region']
+        home_regions = udf[udf['label'] == 'home']['region'].unique().tolist()
         workidxs = during_work(udf)\
-            .groupby(['region']).size().nlargest(2) \
+            .groupby(['region']).size().nlargest(4) \
             .index.tolist()
-        if len(workidxs) == 1 and workidxs[0] == home_region:
+        for homeidx in home_regions:
+            if homeidx in workidxs:
+                workidxs.remove(homeidx)
+        if len(workidxs) == 0:
             print("find work place: not enough visits")
             continue
-        workidx = workidxs[0] if workidxs[0] != home_region else workidxs[1]
-        dfc.loc[(uid, workidx), 'label'] = "work"
+        else:
+            dfc.loc[(uid, workidxs[0]), 'label'] = "work"
         i += 1
         if i % 100 == 0:
             print("done with {}".format(i))
