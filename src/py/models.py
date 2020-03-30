19c2caa7b82451c6c8d3f277266da56f1f602163
Kristoffer Ek <kristofferboek@gmail.com>
2020-03-30 15:46:09 +0200

    Fix visit timeslots in model

--- src/py/models.py
+++ src/py/models.py
@@ -260,7 +260,7 @@ class Sampler:
 
                 for timeslot in range(self.daily_trips_sampling.sample()):
                     current = self.model.next(prev)
-                    samples.append([uid, day, 0] + current)
+                    samples.append([uid, day, timeslot] + current)
                     prev = current
 
             n_done += 1
